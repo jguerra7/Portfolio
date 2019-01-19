675c8091fbe8ed225d56ebbca964b748a7cea9b2
rp729 <44709247+rp729@users.noreply.github.com>
2019-01-19 13:40:36 -0500

    Update ssh_file_transfer.sh

--- Penetration_Scripts/ssh_file_transfer.sh
+++ Penetration_Scripts/ssh_file_transfer.sh
@@ -5,6 +5,6 @@
 read -p "Enter name of file:" file
 read -p "Enter name of user:" user
 read -p "Enter ip address of user:" ip
-read -p "Enter file location:" location
+read -p "Enter target file location:" location
 
 scp $file $user@$ip:$location
