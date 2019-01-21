07c0c8813f7509384b2186b9c724e723386be9bf
rp729 <44709247+rp729@users.noreply.github.com>
2019-01-21 15:58:43 -0600

    Add files via upload

---
+++ Penetration_Scripts/john_the_ripper_windoge.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+
+rm -rf /root/.john
+read -p "Enter location of wordlist :" wordlist
+read -p "Enter loaction of file containing password hash(s) :" list
+
+john --format=NT $list --wordlist=$wordlist
