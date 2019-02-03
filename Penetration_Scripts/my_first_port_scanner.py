5219e46c920f842500f3659b2fb87baef5ccedd6
rp729 <44709247+rp729@users.noreply.github.com>
2019-02-02 22:55:26 -0600

    Update my_first_port_scanner.py

--- Penetration_Scripts/my_first_port_scanner.py
+++ Penetration_Scripts/my_first_port_scanner.py
@@ -3,19 +3,19 @@
 import socket
 
 print ('Welcome to the Python3 port scanner!')
-sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = input("Enter ip address for port scan :")
-port = [20,21,22,23,80,135,139,445]
+tcp_connect = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+udp_connect = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+port_input = int(input("Enter number of ports you with to scan :"))
+port1 = range(1,port_input+1)
+ip = input("Enter ip address you wish to scan :")
 
-def pscan(port):
+def tcp_port_scan(port):
     try:
-        sk.connect((ip,port))
+        con = tcp_connect.connect((ip,port))
         return True
     except:
         return False
 
-for i in port:
-    if pscan(i):
-        print('PORT',i,'IS OPEN!')
-    else:
-        print('port',i,'PWND!')
+for i in port1:
+    if tcp_port_scan(i):
+        print("Port ",i," is OPEN!")
